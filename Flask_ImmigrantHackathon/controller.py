f1472f9a7edb0924c756512b29f95b535bcd1f8b
Ki-Hoon Kang <z16844@gmail.com>
2017-07-15 17:32:42 -0400

    .

--- Flask_ImmigrantHackathon/controller.py
+++ Flask_ImmigrantHackathon/controller.py
@@ -5,16 +5,47 @@ from flask import request, render_template,  jsonify
 from werkzeug import secure_filename
 from Flask_ImmigrantHackathon import app, mysql, UPLOAD_PATH
 from model import GeoLocation, Messege
-import os,math
+import requests,httplib,urllib,time
+import os,math, json
 import pdb
 
+GOOGLE_MAPS_API_KEY = "AIzaSyBA_jfZcH5OvgwsA2ThIzf7drDaTKzKiAk"
+
+PUSHOVER_USERKEY = "utTptvSpVy69q1u4wyj9sm4zAuwr9p"
+PUSHOVER_APIKEY = "avq9vguffqyibm9zb6b5xmb9gehe5o"
+
+def alert(title, messages):
+    conn = httplib.HTTPSConnection("api.pushover.net:443")
+    conn.request("POST","/1/messages.json",
+            urllib.urlencode({
+                "token": PUSHOVER_APIKEY,
+                "user":PUSHOVER_USERKEY ,
+                "title":title,
+                "message": messages,
+                "priority":1,
+                "timestamp":int(time.time()),
+                #'sound': sound
+                }),{"Content-type":"application/x-www-form-urlencoded"})
+    conn.getresponse()
+
 def GetDistance(p1,p2):
-	return math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
+	url = "https://maps.googleapis.com/maps/api/distancematrix/json"
+	querystring = {"key":GOOGLE_MAPS_API_KEY,"origins":str(p1[0]) + "," + str(p1[1]),"destinations":str(p2[0]) + "," + str(p2[1])}
+	response = requests.request("GET", url, params=querystring).json()
+	if response["rows"][0]["elements"][0]["status"] == "ZERO_RESULTS":
+		return None
+	else:
+		return response["rows"][0]["elements"][0]["distance"]
+
 def GetClosestObject(p1,GeoLocList):
 	result = {}
 	for item in GeoLocList:
-		result[GetDistance((float(p1.Latitude),float(p1.Longitude)),(float(item.Latitude),float(item.Longitude)))] = item;
+		DistanceInfo = GetDistance((float(p1.Latitude),float(p1.Longitude)),(float(item.Latitude),float(item.Longitude)))
+		if DistanceInfo is None:
+			continue
+		result[float(DistanceInfo["value"])] = (DistanceInfo["text"], item);
 	closestDistance = min(result.keys())
+	
 	return result[closestDistance]
 
 @app.route('/',methods=["GET","POST"])
@@ -36,7 +67,7 @@ def index():
 			newPost = Messege()
 			newPost.Messege = request.form["Messege"]
 			newPost.Auther = request.form["Auther"]
-			newPost.Anonymous = request.form["Anonymous"] == 1
+			# newPost.Anonymous = request.form["Anonymous"] == 1
 			newPost.GeoLocationKey = newGeoLoc.GeoLocationKey
 			if "files" in request.files:
 				fileList = []
@@ -48,6 +79,7 @@ def index():
 			SQL = """INSERT INTO Messege (`Messege`, `Auther`, `attachmentFiles`, `anonymous`, `GeolocationKey`) VALUES ( '%s', '%s', '%s', '%d', %d); """ % (newPost.Messege,newPost.Auther,newPost.AttachmentFiles,newPost.Anonymous,newPost.GeoLocationKey)
 			cursor.execute(SQL)
 			conn.commit()
+			alert(newPost.Auther,newPost.Messege) # to proof concept
 			return "success"
 	except Exception:
 		return InternalError()
@@ -73,10 +105,11 @@ def closestSpot():
 		result.append(tmpModel)
 	ClosestPoint = GetClosestObject(currentPosition,result)
 
-	SQL = "SELECT * FROM Messege WHERE GeoLocationKey=%d" % ClosestPoint.GeoLocationKey
+	distanceKM = float(ClosestPoint[0].split(' ')[0])
+
+	SQL = "SELECT * FROM Messege WHERE GeoLocationKey=%d" % ClosestPoint[1].GeoLocationKey
 	cursor.execute(SQL)
 
-	pdb.set_trace()
 	columns = tuple( [d[0] for d in cursor.description] )
 	rows = []
 	for row in cursor:
@@ -84,7 +117,9 @@ def closestSpot():
 
 	row = rows[0]
 
-	Model = {"Messege":row["Messege"],"Auther":row["Auther"],"attachmentFiles":row["attachmentFiles"],"anonymous":row["anonymous"],"Latitude":ClosestPoint.Latitude,"Longitude":ClosestPoint.Longitude, }
+	if(distanceKM<1.0):
+		alert(row["Auther"],row["Messege"])
+	Model = {"Messege":row["Messege"],"Auther":row["Auther"],"attachmentFiles":row["attachmentFiles"],"anonymous":row["anonymous"],"Latitude":ClosestPoint[1].Latitude,"Longitude":ClosestPoint[1].Longitude,"Distance":ClosestPoint[0]}
 	return jsonify(Model)
 
 
